c92bee455f307790de8a5da56ae38b63ecdec2dc
aliahmed204 <ali.ah.mo.204@gmail.com>
2024-06-08 14:57:25 +0300

    Create Custom Routes Files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,30 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        using: function(){
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/admin.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/seller.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/client.php'));
+
+            /*require __DIR__.'/../routes/admin.php';
+            require __DIR__.'/../routes/seller.php';
+            require __DIR__.'/../routes/client.php';  */
+        },
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        health: '/up'
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
---
+++ routes/admin.php
@@ -0,0 +1,4 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::view('/example-pages', 'example-page');
